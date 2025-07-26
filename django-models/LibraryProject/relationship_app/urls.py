4d1db20aafcdf4dbc296191922e2e527c333eb49
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-07-26 03:45:58 +0300

    Add role-based HTML templates for Admin, Librarian, and Member views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,3 +13,10 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 ]
+
+
+urlpatterns = [
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+]
