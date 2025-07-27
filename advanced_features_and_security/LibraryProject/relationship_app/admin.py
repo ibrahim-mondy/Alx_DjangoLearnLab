ab44a49998ee61c7fcc6f410c46d99474606ad94
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-07-28 00:18:40 +0300

    Completed custom user model integration

---
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, UserProfile
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(UserProfile)
