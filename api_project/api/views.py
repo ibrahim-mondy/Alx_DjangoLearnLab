f6c306200c3d727add0ba5816645b16ff736df1f
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-07-30 21:49:22 +0300

    done

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,10 @@ from rest_framework import generics
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import responses
 # Create your views here.
 
 class  BookList (generics.ListAPIView):
@@ -13,3 +17,9 @@ class  BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
+class myapiview(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        return responses({'Message':'Hello, authenticated'})
