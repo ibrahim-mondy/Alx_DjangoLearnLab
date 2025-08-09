1ed59d8b391c5b8d43c9cde6a0f3d8d1440f2247
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-10 00:34:59 +0300

    done

--- advanced-api-project/advanced_api_project/api/urls.py
+++ advanced-api-project/advanced_api_project/api/urls.py
@@ -1,7 +1,1 @@
-from django.urls import path
-from .views import AuthorListCreateView, BookListCreateView
 
-urlpatterns = [
-    path('authors/', AuthorListCreateView.as_view(), name='authors-list'),
-    path('books/', BookListCreateView.as_view(), name='books-list'),
-]
