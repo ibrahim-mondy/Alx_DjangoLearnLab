3a92c141b0403b23e0b7a983469f384a438e704a
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-17 01:05:53 +0300

    task 2

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import RegisterForm, ProfileForm
 
-# Create your views here.
+def home_view(request):
+    return render(request, "blog/home.html")
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = RegisterForm()
+    return render(request, "blog/register.html", {"form": form})
+
+@login_required
+def profile_view(request):
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = ProfileForm(instance=request.user)
+    return render(request, "blog/profile.html", {"form": form})
