f26b3e46a69f3908a2c3e63ce8a5a520d03402e1
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-17 00:38:09 +0300

    Done

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,4 +7,3 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oy#zluy%+*e=ufrbm-qu7s$o497xdwgc5)m)a1ekjnsenblm)_'
+SECRET_KEY = 'django-insecure-_(1!gzc#_lyhv^o6uuf$rw&y9syj%j-l_q7f_)!a$ij5xgj9d)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,7 +119,6 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    BASE_DIR / "blog" / "static",
 ]
 
 # Default primary key field type
