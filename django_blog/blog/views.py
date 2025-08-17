a0adc5bf755bd1c6adb6a4b7a650e6da502f9c0d
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-18 00:57:53 +0300

    .

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,33 +1,47 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
 
 class PostListView(ListView):
     model = Post
-    template_name = "blog/post_list.html"
-    context_object_name = "posts"
+    template_name = 'blog/post_list.html'
+
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = "blog/post_detail.html"
+    template_name = 'blog/post_detail.html'
 
-class PostCreateView(CreateView):
+
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    template_name = "blog/post_form.html"
-    fields = ["title", "content", "author"]
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
 
-    def get_success_url(self):
-        return reverse_lazy("post_list")
+    def form_valid(self, form):
+        # يربط البوست بالمستخدم الحالي
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-class PostUpdateView(UpdateView):
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    template_name = "blog/post_form.html"
-    fields = ["title", "content"]
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
 
-    def get_success_url(self):
-        return reverse_lazy("post_list")
+    def test_func(self):
+        # يسمح بس للكاتب بتعديل البوست
+        post = self.get_object()
+        return self.request.user == post.author
 
-class PostDeleteView(DeleteView):
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = "blog/post_confirm_delete.html"
-    success_url = reverse_lazy("post_list")
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
+
+    def test_func(self):
+        # يسمح بس للكاتب بحذف البوست
+        post = self.get_object()
+        return self.request.user == post.author
