4e2331343ca486415926974ab79e4f567f7a93e3
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-21 00:33:36 +0300

    update

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "accounts.CustomUser"
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ]
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+    "DEFAULT_FILTER_BACKENDS": ["rest_framework.filters.SearchFilter"],
 }
--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -21,8 +21,11 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
+    path("api/", include("posts.urls")),
     
 ]
+
+
 
 
 
