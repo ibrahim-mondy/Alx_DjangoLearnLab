232b94dc1c3ba770dcb7b1472d81063965c00f35
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-21 00:52:50 +0300

    update

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,6 +4,11 @@ from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from .models import CustomUser
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
@@ -30,3 +35,16 @@ class LoginView(APIView):
             "user": UserSerializer(user).data,
             "token": token.key
         }, status=status.HTTP_200_OK)
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(CustomUser, id=user_id)
+    request.user.following.add(user_to_follow)
+    return Response({"message": f"You are now following {user_to_follow.username}"})
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+    request.user.following.remove(user_to_unfollow)
+    return Response({"message": f"You unfollowed {user_to_unfollow.username}"})
