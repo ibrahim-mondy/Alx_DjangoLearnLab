b8ab7d4c98420463b23e8773bf0dadffb70f0f12
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-21 00:34:26 +0300

    update

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -128,4 +128,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5,
     "DEFAULT_FILTER_BACKENDS": ["rest_framework.filters.SearchFilter"],
+    
 }
