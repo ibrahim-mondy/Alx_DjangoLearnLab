342813b33689db318bc7338dfcc5c78b4385cdfa
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-20 22:12:47 +0300

    update

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy
 from .models import Post, Comment
 from .forms import CustomUserRegistrationForm, ProfileUpdateForm
+from .forms import CommentForm
 
 
 class PostListView(ListView):
@@ -48,3 +49,42 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         
         post = self.get_object()
         return self.request.user == post.author
+    
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post-detail', pk=post.id)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment.html', {'form': form})
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/edit_comment.html"
+
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'pk': self.object.post.id})
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = "blog/delete_comment.html"
+
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'pk': self.object.post.id})
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
