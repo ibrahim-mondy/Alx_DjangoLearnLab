f283e15f5978997fd73acb387946f33913fd6b09
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-21 00:33:56 +0300

    update

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -21,8 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
-    path("api/", include("posts.urls")),
-    
+    path("api/", include("posts.urls")), 
 ]
 
 
