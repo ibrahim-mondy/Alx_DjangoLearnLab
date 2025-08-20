4ea344f6f14188e64f5f6e278e45b979dd3f7245
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-21 00:59:56 +0300

    update

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -16,9 +16,8 @@ router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path("", include(router.urls)),
 ]
-from .views import follow_user, unfollow_user
-
+from .views import FollowUserView, UnfollowUserView
 urlpatterns = [
-    path("follow/<int:user_id>/", follow_user, name="follow-user"),
-    path("unfollow/<int:user_id>/", unfollow_user, name="unfollow-user"),
+    path("follow/<int:user_id>/", FollowUserView.as_view(), name="follow-user"),
+    path("unfollow/<int:user_id>/", UnfollowUserView.as_view(), name="unfollow-user"),
 ]
