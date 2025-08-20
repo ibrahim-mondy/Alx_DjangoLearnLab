0b56aaee0c6c02b71f97f07b219171081bfb5f9b
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-08-20 22:38:07 +0300

    update

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -160,3 +160,19 @@ class TagPostListView(ListView):
         context = super().get_context_data(**kwargs)
         context["tag"] = self.tag
         return context
+    
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        tag_slug = self.kwargs.get("tag_slug")
+        return Post.objects.filter(tags__slug=tag_slug)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_slug = self.kwargs.get("tag_slug")
+        context["tag"] = Tag.objects.get(slug=tag_slug)
+        return context
